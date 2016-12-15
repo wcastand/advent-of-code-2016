b559e3bf16c2bd2cc2cf81229400b63c3c92fccf
William Castandet <wcastand@gmail.com>
2016-12-15 17:30:56 +0100

    exo 5.1, perf pb i thin

--- exo5.js
+++ exo5.js
@@ -4,9 +4,18 @@ import md5 from 'md5'
 const debug = R.tap(console.log)
 
 export function exo51 (path) {
-  const notZero = R.either(
-    R.gt(R.__, 100000),
-    R.compose(R.equals('00000'), R.slice(0, 4), x => md5(path + x.toString()))
-  )
-  return R.until(notZero, R.inc, 0)
+  const notZero = R.compose(R.equals('00000'), R.slice(0, 5), x => md5(path + x.toString()))
+
+  // take the index to start
+  const calculateKey = R.curry((idx, acc) => R.compose(
+    x => [x, acc + R.prop(5, md5(path + x))],
+    R.until(notZero, R.inc)
+  )(idx))
+  return R.compose(
+    R.prop(1),
+    R.until(
+      R.compose(R.equals(8), R.length, R.prop(1)),
+      R.converge(calculateKey, [R.compose(R.inc, R.prop(0)), R.prop(1)])
+    )
+  )([3000000, ''])
 }
--- exo5.test.js
+++ exo5.test.js
@@ -4,10 +4,18 @@ import { exo51 } from './exo5'
 
 test('test exo 5.1', () => {
   const path = 'abc'
+  var start = clock()
   expect(exo51(path)).toBe('18f47a30')
+  var duration = clock(start)
+  console.log("Took "+duration+"ms")
 })
 
 // test('test real exo 5.1', () => {
 //   const path = 'cxdnnyjw'
 //   expect(exo51(path)).toBe(361724)
 // })
+function clock(start) {
+   if ( !start ) return process.hrtime()
+   var end = process.hrtime(start)
+   return Math.round((end[0]*1000) + (end[1]/1000000))
+}
